27fa8764bd7e410c16dae5e4924cb588f7972955
Matt Stofko <matt@mjslabs.com>
2012-10-18 02:01:20 -0700

    Add licensing info

--- main.c
+++ main.c
@@ -5,6 +5,22 @@
 	Matt Stofko <matt@mjslabs.com>
 */
 
+/*
+COPYRIGHT & LICENSE
+
+Copyright 2006 Matt Stofko
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of either:
+
+* the GNU General Public License as published by the Free
+Software Foundation; either version 1, or (at your option) any
+later version, or
+
+* the Artistic License version 2.0.
+
+*/
+
 #include <stdio.h>
 #include <Carbon/Carbon.h>
 
